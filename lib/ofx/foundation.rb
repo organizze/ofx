6c9bafa24f95a2d62fceb2370eb5d6608439b8bb
Esdras Mayrink <falecom@oesdras.com.br>
2015-08-13 15:19:41 -0300

    making it easy to render as json

--- lib/ofx/foundation.rb
+++ lib/ofx/foundation.rb
@@ -1,6 +1,8 @@
 module OFX
   class Foundation
+    attr_reader :as_json
     def initialize(attrs)
+      @as_json = attrs
       attrs.each do |key, value|
         send("#{key}=", value)
       end
