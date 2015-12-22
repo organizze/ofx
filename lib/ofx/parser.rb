1834501016f92dbea51098ec23e8ba1a5105e687
Esdras Mayrink <falecom@oesdras.com.br>
2015-12-22 11:21:14 -0200

    Add support to 103 OFX version

--- lib/ofx/parser.rb
+++ lib/ofx/parser.rb
@@ -17,7 +17,7 @@ module OFX
         end
 
         case headers["VERSION"]
-        when /102/ then
+        when /102|103/ then
           @parser = OFX102.new(:headers => headers, :body => body)
         when /200|211/ then
           @parser = OFX211.new(:headers => headers, :body => body)
